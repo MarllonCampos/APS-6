586b981710f89b96d81ec7be255a022ae469d9ff
MarllonCampos <marlloncampos@outlook.com>
2020-09-25 20:25:27 -0300

    Backend funcionando recebendo arquivos sem problemas, falta fazer o front end enviar o video de uma forma que o backend entenda

--- Marllon/server.js
+++ Marllon/server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const https = require('https')
 const path = require('path')
 const fs = require('fs')
+const multer = require('multer')
+const upload = multer()
 
 
 const app = express()
@@ -11,17 +13,22 @@ const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 
+
 app.get('/',(req,res)=>{
     res.send('Server Running!')
 })
 
-app.post('/sendPhoto',(req,res)=>{
-    console.log(req)
-    res.send('running')
+app.post('/sendVideo',upload.single("file"),(req,res)=>{
+    console.log(req.file)
+    res.json({ok:"sei la"})
+
+
 })
 
 keyServer = path.resolve(__dirname,'serveFiles','certificates','server.key')
 certServer = path.resolve(__dirname,'serveFiles','certificates','server.cert')
+
+
 
 https.createServer({
     key:fs.readFileSync(keyServer),
