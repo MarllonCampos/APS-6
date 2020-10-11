333ac92870520411de07c0ce7596008793172985
MarllonCampos <marlloncampos@outlook.com>
2020-10-10 21:31:15 -0300

    Criado novo estilo para os agrotóxicos que serão inseridos, inseridos agrotóxicos na pagina

--- Marllon/serveFiles/public/scripts/showInScreen.js
+++ Marllon/serveFiles/public/scripts/showInScreen.js
@@ -1,6 +1,33 @@
 if (localStorage.getItem("conteudoAps")){
-    conteudoBanco = localStorage.getItem("conteudoAps")
-    console.log(conteudoBanco)
+    const nomeUsuario = document.querySelector('.nomeus h4')
+    const informacoes = document.querySelector('.informacoes')
+    conteudoBanco = JSON.parse(localStorage.getItem("conteudoAps"))
+    nomeBanco = localStorage.getItem("nomeConteudoAps")
+    nomeUsuario.innerText = nomeBanco
+    
+    conteudoBanco.map(e => {
+        const divEl = document.createElement('div')
+        const nomeAgrotoxico= document.createElement('h4')
+        const classeAgrotoxico= document.createElement('h4')
+        const tipoAgrotoxico= document.createElement('h4')
+        const periculosidadeAgrotoxico= document.createElement('h4')
+        divEl.className = "infonomes"
+
+        nomeAgrotoxico.innerText = e.nome
+        classeAgrotoxico.innerText = e.classe
+        tipoAgrotoxico.innerText = e.tipoAplicacao
+        periculosidadeAgrotoxico.innerText = e.periculosidade
+
+        divEl.appendChild(nomeAgrotoxico)
+        divEl.appendChild(classeAgrotoxico)
+        divEl.appendChild(tipoAgrotoxico)
+        divEl.appendChild(periculosidadeAgrotoxico)
+        
+        informacoes.appendChild(divEl)
+
+    })
+
+    
 
 }else{
     alert('Você não esta logado, voltando para a pagina inicial!!')
